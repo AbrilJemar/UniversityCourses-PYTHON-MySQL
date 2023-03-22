b6733cfd599e93fc9cb194544e2dba2d367ba8c3
Abril Jemar <121254254+AbrilJemar@users.noreply.github.com>
2023-03-22 00:54:44 -0300

    Update courses.sql

--- courses.sql
+++ courses.sql
@@ -3,10 +3,14 @@ USE university;
 
 CREATE TABLE courses(
 	id_course varchar(100),
-	course_name varchar(100),
-	course_schedule varchar(100),
-	primary key(id_course)
+    	course_name varchar(100),
+    	course_schedule varchar(100),
+    	primary key(id_course)
 );
+
+INSERT INTO courses(id_course, course_name, course_schedule) VALUES ("K1234", "Mathematical analysis", "Monday from 7:00 p.m. to 11:00 p.m.");
+INSERT INTO courses(id_course, course_name, course_schedule) VALUES ("K4321", "Biology", "Thursday from 7:00 p.m. to 11:00 p.m.");
+INSERT INTO courses(id_course, course_name, course_schedule) VALUES ("56783", "Economy", "Wednesday from 7:00 p.m. to 11:00 p.m.");
 
 SELECT * FROM courses;
 
