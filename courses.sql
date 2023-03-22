09f260bcb6076547b764075491b38cbb5aef1f90
Abril Jemar <121254254+AbrilJemar@users.noreply.github.com>
2023-03-21 23:48:21 -0300

    Add files via upload

---
+++ courses.sql
@@ -0,0 +1,12 @@
+use university;
+
+CREATE TABLE courses(
+	id_course varchar(20),
+    course_name varchar(100),
+    course_schedule varchar(20),
+	
+    primary key(id_course)
+);
+
+SELECT * FROM courses;
+
