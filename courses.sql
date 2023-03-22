0267e5c88ae6c505779bf0d5d46fab3646fe2480
Abril Jemar <121254254+AbrilJemar@users.noreply.github.com>
2023-03-21 23:52:00 -0300

    Update courses.sql

--- courses.sql
+++ courses.sql
@@ -1,11 +1,11 @@
-use university;
+CREATE DATABASE university;
+USE university;
 
 CREATE TABLE courses(
-	id_course varchar(20),
-    course_name varchar(100),
-    course_schedule varchar(20),
-	
-    primary key(id_course)
+	id_course varchar(100),
+	course_name varchar(100),
+	course_schedule varchar(100),
+	primary key(id_course)
 );
 
 SELECT * FROM courses;
