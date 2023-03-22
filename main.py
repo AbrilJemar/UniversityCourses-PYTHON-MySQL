2882b4996692eeec2bd3a65265c6edaaa71b8931
Abril Jemar <121254254+AbrilJemar@users.noreply.github.com>
2023-03-22 00:40:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def App():
         
         match option:
             
+            ################################## List courses #######################################
             case 1:
                 Courses = DAO.listCourses()
                 
@@ -26,24 +27,16 @@ def App():
                     functions.ListCourses(Courses, Columns)
 
                 else:
-                    Exit = input('''\nNo course found. Go back to main menu?
-1) YES
-2) NO
-
-Choose an option: ''')
-                    Exit = functions.check_user_input(Exit)
-        
-                    while(isinstance(Exit, str) or Exit not in [1,2]):
-                        Exit = input("Invalid option. Try again: ")
-                        Exit = functions.check_user_input(Exit)    
+                    Exit = functions.GoBack()
 
                     if Exit == 2:
                         print('Thanks for using this program')
-                        break
-
+                        break  
                 
 
-        
+                
+                
+            ################################## Add a course #######################################
             case 2:
                 
                 Columns = DAO.listColumns()
@@ -51,89 +44,56 @@ Choose an option: ''')
                 DAO.addCourse(NewCourse, Columns)    
 
 
-
                 
+
+            ################################## Edit a course ######################################
             case 3:
                 try:
-                    DAO.EditCourse(NewCourse)    
-
-
+                     #it's not ready yet
                 except:
                     print('ERROR')
 
 
+                    
 
-
-
+            ################################## Remove a course ####################################
             case 4:
                 try:
                     courses = DAO.listCourses()
                     if len(courses) > 0:
-                        sure = input('''Are you sure you want to delete a course?
-1) YES.
-2) NO.
-
-Choose an option: ''')
-                        sure = functions.check_user_input(sure)
-                        while(isinstance(sure, str) or sure not in [1,2]):
-                            sure = input("Invalid option. Try again: ")
-                            sure = functions.check_user_input(sure)    
-
+                        sure = functions.SureDelete()
                         if sure == 1:
                             columns = DAO.listColumns()
                             IdCourse = functions.DataDeleteCourse(courses, columns)
                             DAO.deleteCourse(IdCourse)
 
-                        
                     else:
-                        Exit = input('''\nNo courses to drop. Go back to main menu?
-1) YES.
-2) NO.
-
-Choose an option: ''')
-                        Exit = functions.check_user_input(Exit)
-        
-                        while(isinstance(Exit, str) or Exit not in [1,2]):
-                            Exit = input("Invalid option. Try again: ")
-                            Exit = functions.check_user_input(Exit)    
-
+                        Exit = functions.GoBack()
                         if Exit == 2:
                             print('Thanks for using this program')
                             break
-
-
-
+                            
                 except:
                     print('ERROR')
 
+                    
 
 
-
+            ################################## Search a course ###################################
             case 5:
                 try:
-                    '''Columns = DAO.listColumns()
-                    functions.ListColumns(Columns)'''
-                      
-
-
+                    #it's not ready yet
                 except:
                     print('ERROR')
 
+ 
 
+
+            ################################## Modify columns ####################################
             case 6:
                 while True:
-                    action = input('''\nWhat do you want to modify?:
-1) Add a new column.
-2) Delete a column.
-3) Go back to main menu.                          
 
-''')
-
-                    action = functions.check_user_input(action)
-                    while(isinstance(action, str) or action not in [1,2,3]):
-                        action = input("Invalid option. Try again: ")
-                        action = functions.check_user_input(action) 
-
+                    action = functions.Modify()
                 
                     match action:
                         case 1:
@@ -148,21 +108,13 @@ Choose an option: ''')
                             break
 
 
+                            
+                            
+            ################################## Close program #####################################
             case 7: 
                 print('\r\n \n \n~~~~~~~~~~~    OFF    ~~~~~~~~~~~~')
                 break
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 App()
+
